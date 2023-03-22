cc9afa2948e8d0cc49e2efb0bc7be9bfc6b43080
Filip Skřiváček <filskri019@pslib.cz>
2023-03-22 23:24:21 +0100

    Final

--- Pages/HomeScreen.jsx
+++ Pages/HomeScreen.jsx
@@ -1,49 +1,80 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, StyleSheet, FlatList } from "react-native";
+import * as Font from 'expo-font';
 import CustomButton from "../components/CustomButton";
 import { useNavigation } from "@react-navigation/native";
 
 const HomeScreen = () => {
   const navigation = useNavigation();
+  const [fontsLoaded, setFontsLoaded] = useState(false);
   const [events, setEvents] = useState([
     {
       name: "Pivní slavnosti",
       location: "Pivovar Vratislavice",
-      time: "21.4. 19:30",
+      start: "21.4. 19:30",
+      end: "22.4 3:00",
+      fee: "50,-",
       key: "1",
+      note: "Největší pivní slavnosti v Liberci!",
     },
     {
       name: "Maturitní ples P4",
       location: "Kulturní dům Liberec",
-      time: "22.6. 19:00",
+      start: "22.6. 19:00",
+      end: "23.6. 4:00",
+      fee: "300,-",
       key: "2",
+      note: "Taneční zábava pro všechny maturanty P4!",
     },
     {
       name: "Liberecký trh",
       location: "Liberecké náměstí",
-      time: "13.7. 15:00",
+      start: "13.7. 15:00",
+      end: "13.7. 20:00",
+      fee: "Zadarmo",
       key: "3",
+      note: "Prodejní stánky s místními specialitami a řemeslnými výrobky.",
     },
     {
       name: "Velikonoční trhy",
       location: "Náměstí Ž. Brod",
-      time: "Velikonoce",
+      start: "8.4 13:00",
+      end: "8.4 17:00",
+      fee: "Dobrovolné",
       key: "4",
+      note: "Tradiční velikonoční trhy s pestrou nabídkou.",
     },
     {
       name: "Maturitní ples E4",
       location: "Kulturní dům Liberec",
-      time: "16.9. 18:00",
+      start: "16.9. 18:00",
+      end: "17.9 4:00",
+      fee: "300,-",
       key: "5",
+      note: "Elegantní taneční večer pro maturanty E4!",
     },
   ]);
+
+  useEffect(() => {
+    async function loadFonts() {
+      await Font.loadAsync({
+        'Bebas Neue': require('../Fonts/BebasNeue-Regular.ttf'),
+      });
+      setFontsLoaded(true);
+    }
+    loadFonts();
+  }, []);
+
+  if (!fontsLoaded) {
+    return null;
+  }
 
   const renderItem = ({ item }) => (
     <View style={styles.itemContainer}>
       <View style={styles.item}>
         <Text style={styles.itemName}>{item.name}</Text>
         <Text style={styles.itemLocation}>{item.location}</Text>
-        <Text style={styles.itemTime}>{item.time}</Text>
+        <Text style={styles.itemstart}>{item.start}</Text>
       </View>
       <View style={styles.buttonContainer}>
         <CustomButton
@@ -69,7 +100,6 @@ const HomeScreen = () => {
         keyExtractor={(item) => item.key}
         showsVerticalScrollIndicator={false}
       />
-      
     </View>
   );
 };
@@ -106,7 +136,7 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontFamily: "Bebas Neue",
   },
-  itemTime: {
+  itemstart: {
     fontSize: 16,
     color: "gray",
     fontFamily: "Bebas Neue",
